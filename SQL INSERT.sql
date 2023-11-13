5ca5804a576717bc282c985313128927810daea3
KarolinaGierach <80774132+KarolinaGierach@users.noreply.github.com>
2023-11-13 11:32:11 +0100

    Create SQL INSERT.sql

---
+++ SQL INSERT.sql
@@ -0,0 +1,17 @@
+--1. Insert a new record in the Customers table.
+
+
+INSERT INTO
+ Customers (
+CustomerName, 
+Address, 
+City, 
+PostalCode,
+Country)
+
+VALUES
+('Hekkan Burger',
+'Gateveien 15',
+'Sandnes',
+'4306',
+'Norway');
