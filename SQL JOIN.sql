b1f9619ef6ca2db05562cd5046a91195c72a9933
KarolinaGierach <80774132+KarolinaGierach@users.noreply.github.com>
2023-11-13 14:08:29 +0100

    Create SQL JOIN.sql

---
+++ SQL JOIN.sql
@@ -0,0 +1,2 @@
+-- 1. Insert the missing parts in the JOIN clause to join the two tables Orders and Customers, using the CustomerID field in both tables as the relationship between the two tables.
+
